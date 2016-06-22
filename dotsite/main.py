6dc0570ca2a627fd329592d675ef59c2b34b062d
J Alan Brogan <github@al-got-rhythm.net>
2016-06-22 05:20:48 +0100

    Provide decorators to handle ArgParser

--- dotsite/main.py
+++ dotsite/main.py
@@ -4,7 +4,6 @@ Usage:
 
 import dotsite as site
 
-@site.streamer
 def main(streams):
     for stream in streams:
         if stream.read() in 'qQ':
@@ -14,22 +13,13 @@ def main(streams):
 sys.exit(site.main(main, __name__))
 """
 
-def streamer(main_method):
-    """Open a stream for the first file in arguments, or stdin"""
-    if not arguments:
-        return [sys.stdin]
-    elif arguments[0] == '-c':
-        return [StringIO(get_clipboard_data())]
-    for argument in arguments:
-        if os.path.isfile(argument):
-            return file(argument, 'r')
-    return []
+@site.decorators.streamer
+def main(streams):
+    for stream in streams:
+        if stream.read() in 'qQ':
+            break
 
 
-def main(method, name):
-    if name != '__main__':
-        return
-    stream = streamer(sys.argv)
-
-if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+def exit(method, name):
+    if __name__ == '__main__':
+        sys.exit(method(sys.argv))
