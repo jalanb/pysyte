0778b756ecf9a939819c5ba17f7e5221ee73773d
J Alan Brogan <github@al-got-rhythm.net>
2016-11-13 00:23:46 +0000

    Depend less directly on args

--- dotsite/scripts.py
+++ dotsite/scripts.py
@@ -13,12 +13,14 @@ _versions = [__version__]
 _exit_ok = os.EX_OK
 _exit_fail = not _exit_ok
 
+args = None
+
 
 def latest_version():
     return _versions[-1]
 
 
-def version(args):
+def version():
     print('%s %s' % (args, latest_version()))
     raise SystemExit
 
@@ -26,20 +28,18 @@ def version(args):
 def parse_args(add_args, docstring):
     """Parse out command line arguments"""
 
-    def run_args(args):
+    def run_args():
         """Run any global methods eponymous with args"""
         valuable_args = {k for k, v in args.__dict__.items() if v}
         arg_methods = {globals()[a] for a in valuable_args if a in globals()}
         if not arg_methods:
             return
         for method in arg_methods:
-            method(args)
+            method()
         raise SystemExit(_exit_ok)
 
     parser = argparse.ArgumentParser(
-        description=docstring
-        and docstring.splitlines()[0]
-        or "No docstring provided")
+        description=docstring and docstring.splitlines()[0] or "No docstring")
     result = add_args(parser)
     parser = result if result else parser
     parser.add_argument('-v', '--version', action='store_true',
@@ -48,8 +48,9 @@ def parse_args(add_args, docstring):
     # help='Do not show stdout')
     # parser.add_argument('-Q', '--quiet_errors', action='store_true',
     # help='Do not show stderr')
+    global args  # pylint: disable=global-statement
     args = parser.parse_args()
-    run_args(args)
+    run_args()
     return args
 
 
@@ -73,6 +74,9 @@ def main(method, add_args, version=None,
         _versions.append(version)
     try:
         args = parse_args(add_args, docstring)
+        args = parse_args(
+            add_args,
+            docstring and docstring or '%s()' % method.__name__)
         return _exit_ok if method(args) else _exit_fail
     except KeyboardInterrupt as e:
         ctrl_c = 3
@@ -81,7 +85,7 @@ def main(method, add_args, version=None,
         return e.code
     except DebugExit:
         return _exit_fail
-    except Exception, e:  # pylint: disable=broad-except
+    except Exception as e:  # pylint: disable=broad-except
         if int(latest_version().split('.')[0]) < 1:
             raise
         if error_stream:
