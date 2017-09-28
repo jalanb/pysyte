24586c8574bf56c0e12a92c410d2a0b5478c216e
J Alan Brogan <github@al-got-rhythm.net>
2017-09-28 01:59:33 +0100

    Undo bad commit

--- dotsite/platforms.py
+++ dotsite/platforms.py
@@ -1,15 +1,17 @@
 import platform as python_platform
 
 import dotsite
+from dotsite import imports
+from dotsite import cmnds
 
 _platform_name = python_platform.system().lower()
-platform = dotsite.imports.load_module(dotsite, _platform_name)
+platform = imports.load_module(dotsite, _platform_name)
 
 
 def _bash(command):
     if not command:
         return None
-    status, output = dotsite.cmnds.getstatusoutput(command)
+    status, output = cmnds.getstatusoutput(command)
     if status:
         raise ValueError(output)
     return output
