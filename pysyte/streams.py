a022c4f70f955a0fe8d5d83c3e1b066a2bc00d75
J Alan Brogan <github@al-got-rhythm.net>
2019-04-14 16:06:05 +0100

    Yield swallow std streams as strings

--- pysyte/streams.py
+++ pysyte/streams.py
@@ -8,22 +8,34 @@ from six import StringIO
 
 
 @contextmanager
+def swallow_stdout_stderr():
+    """Divert stdout, stderr to strings"""
+    saved_out = sys.stdout
+    saved_err = sys.stderr
+    sys.stdout = StringIO()
+    sys.stderr = StringIO()
+    try:
+        yield sys.stdout.getvalue(), sys.stderr.getvalue()
+    finally:
+        sys.stdout = saved_out
+        sys.stderr = saved_err
+
+
+@contextmanager
 def swallow_stdout(stream=None):
     """Divert stdout into the given stream
 
-    >>> from cStringIO import StringIO
     >>> string = StringIO()
     >>> with swallow_stdout(string):
     ...     print('hello')
-    >>> string.getvalue() == 'hello'
-    True
+    >>> assert string.getvalue() == 'hello'
     """
     saved = sys.stdout
     if stream is None:
         stream = StringIO()
     sys.stdout = stream
     try:
-        yield
+        yield sys.stdout.getvalue()
     finally:
         sys.stdout = saved
 
@@ -35,6 +47,6 @@ def swallow_stderr(stream=None):
         stream = StringIO()
     sys.stderr = stream
     try:
-        yield
+        yield sys.stderr.getvalue()
     finally:
         sys.stderr = saved
