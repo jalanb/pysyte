7a14e0e722b7b3c71089c47fafaf3757bf06e970
J Alan Brogan <github@al-got-rhythm.net>
2019-05-14 02:09:16 +0100

    Test iteration

--- pysyte/iteration.py
+++ pysyte/iteration.py
@@ -8,6 +8,8 @@ def first(sequence, message=None):
     """The first item in that sequence
 
     If there aren't any, raise a ValueError with that message
+
+    >>> assert first([1, 2, 3]) == 1
     """
     try:
         return next(iter(sequence))
@@ -19,6 +21,8 @@ def last(sequence, message=None):
     """The last item in that sequence
 
     If there aren't any, raise a ValueError with that message
+
+    >>> assert last([1, 2, 3]) == 3
     """
     try:
         return sequence.pop()
@@ -29,6 +33,11 @@ def last(sequence, message=None):
 
 
 def first_or(sequence, value):
+    """First item in that sequence, or that value
+
+    >>> assert first_or([1, 2, 3], 4) == 1
+    >>> assert first_or([], 4) == 4
+    """
     try:
         return first(sequence)
     except ValueError:
@@ -39,6 +48,8 @@ def first_that(predicate, sequence, message=None):
     """The first item in that sequence that matches that predicate
 
     If none matches raise a KeyError with that message
+
+    >>> assert first_that(lambda x: x > 1, [1, 2, 3]) == 2
     """
     try:
         return next(ifilter(predicate, sequence))
