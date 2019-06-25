e96f6aefce2b095604a532a0d5cd776ad7c0bb7a
J Alan Brogan <github@al-got-rhythm.net>
2019-06-25 10:31:04 +0100

    Convert scripts to use new cli package

--- pysyte/cli/arguments.py
+++ pysyte/cli/arguments.py
@@ -1,4 +1,7 @@
-"""Handle arguments from the command line"""
+"""Handle arguments from the command line
+
+This module is a simplifying proxy to stdlib's argparse
+"""
 
 import argparse
 import sys
@@ -23,13 +26,13 @@ def extract_strings(names, name):
 def parser(description=None, usage=None):
     """Make a command line argument parser"""
 
-    class ArgNamespace(object):
+    class ArgumentsNamespace(object):
         def __init__(self, result):
             self._result = result
 
         def __getattr__(self, name):
             try:
-                super(ArgNamespace, self).__getattr__(name)
+                super(ArgumentsNamespace, self).__getattr__(name)
             except AttributeError:
                 return getattr(self._result, name)
 
@@ -43,37 +46,37 @@ def parser(description=None, usage=None):
                 return None
             return extract_strings(self._result.__dict__, name)
 
-    class ArgParser(object):
-        def __init__(self, argument_parser):
-            self.parser = argument_parser
+    class ArgumentsParser(object):
+        def __init__(self, usage, epilog):
+            self.parser = argparse.ArgumentParser(usage=usage, epilog=epilog)
             self._parsed = None
 
             self.parse = self.parse_args
-            self.arg = self.parser.add_argument
-            self.opt = self.true = partial(self.arg, action='store_true')
-            self.int = partial(self.arg, type=int)
+
+            self.string = self.arg = self.parser.add_argument
+            self.boolean = self.opt = self.true = partial(self.arg, action='store_true')
+            self.integer = self.int = partial(self.arg, type=int)
 
         def __repr__(self):
             return f'<{self.__class__.__name__}>'
 
-        def parse_args(self, args=None):
-            return ArgNamespace(self.parser.parse_args(args))
-
-        def args(self, *args, **kwargs):
+        def positional(self, *args, **kwargs):
             """Add optional positional args"""
-            return self.arg(*args, **kwargs, nargs='*')
+            return self.string(*args, **kwargs, nargs='*')
 
-        def argss(self, *args, **kwargs):
+        def positionals(self, *args, **kwargs):
             """Add mandatory positional args"""
-            return self.arg(*args, **kwargs, nargs='+')
+            return self.string(*args, **kwargs, nargs='+')
+
+        def parse_args(self, args=None):
+            return ArgumentsNamespace(self.parser.parse_args(args))
 
 
     lines = description.splitlines()
+    epilog = None
     if not usage:
         if len(lines) > 1 and not lines[1]:
             usage, epilog = lines[0], '\n\n'.join(lines[2:])
     else:
         usage = description = lines[0]
-        epilog = None
-    return ArgParser(
-        argparse.ArgumentParser(usage=description))
+    return ArgumentsParser(usage, epilog)
--- pysyte/cli/main.py
+++ pysyte/cli/main.py
@@ -1,34 +1,59 @@
+"""Handle scripts from the command_line
+
+This module is a simplifying proxy to stdlib's sys.exit()
+"""
+
+import inspect
 import os
+import sys
 
 import stackprinter
 
 from . import arguments
 from ..debuggers import DebugExit
 
-_exit_ok = os.EX_OK
-_exit_fail = not _exit_ok
 
-def call_main(main):
-
+def try_main(main):
+    """Handle expected exceptions"""
     try:
-        return _exit_ok if main() else _exit_fail
-    except SystemExit as e:
-        return e.code
+        return os.EX_OK if main() else os.X_OK
     except KeyboardInterrupt as e:
+        sys.stderr.write('^c ^C ^c    ^C ^c ^C    ^c ^C ^c\n')
         ctrl_c = 3
         return ctrl_c
     except DebugExit:
-        return _exit_fail
+        return os.X_OK
+    except SystemExit as e:
+        return e.code
     except Exception as e:  # pylint: disable=broad-except
         stackprinter.show(e, style='darkbg')
 
 
-def run_main(main, args):
-    def inner_main():
-        args = parser.parse_args()
-        return main(args)
+def run(main_method, add_args=None, post_parse=None):
+    """Run a main_method from command line, parsing arguments
 
-    parser = arguments.parser(args.__module__.__doc__)
-    args(parser)
-    sys.exit(call_main(inner_main))
+    if add_args(parser) is given it should add arguments to the parser
+    if post_parse(args) is given then it is called with parsed args
+        if it returns the args they will be used
+    Then call:
+        main_method(args)
+    """
 
+    module = inspect.getmodule(main_method)
+    if not add_args:
+        main = main_method
+        assert not main_method.__code__.co_argcount
+    else:
+        def main():
+            args = parser.parse_args()
+            if post_parse:
+                parsed = post_parse(args)
+                if parsed:
+                    args = parsed
+            return main_method(args)
+
+        parser = arguments.parser(module.__doc__)
+        add_args(parser)
+
+    if module.__name__ == '__main__':
+        sys.exit(try_main(main))
