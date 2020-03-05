4ad952e7d18f832234b2a184c2536a689f246e8f
J Alan Brogan <github@al-got-rhythm.net>
2020-03-05 18:21:44 +0100

    Fix testing typos

--- pysyte/net/hosts.py
+++ pysyte/net/hosts.py
@@ -1,11 +1,16 @@
 """Simplified hosts for pysyte"""
 
+import getpass
+import socket
+from dataclasses import dataclass
+
 @dataclass
 class Host:
     hostname: str
     aliases: list
     addresses: list
     users: list
+
 
 localhost = Host(*(
     list(socket.gethostbyname_ex(socket.gethostname())) +
