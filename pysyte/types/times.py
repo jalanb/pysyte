476b2271ab12c4b42280a0cce91962d93e05e75e
J Alan Brogan <github@al-got-rhythm.net>
2020-04-13 22:04:33 +0100

    Test times

--- pysyte/types/times.py
+++ pysyte/types/times.py
@@ -1,11 +1,30 @@
-"""Handle datetime for pysyte's types"""
+"""Handle datetime for pysyte types"""
+
 import datetime
 epoch = datetime.datetime.fromtimestamp(0)
 
 
-def taken(diff, days=True):
-    seconds = diff.seconds * 1_000 + diff.microseconds
-    if days:
-        return seconds
-    result = abs(diff.days * 24 * 60 * 60 + seconds)
-    return result
+def taken(diff):
+    """Convert a time diff to total number of microseconds"""
+    microseconds = diff.seconds * 1_000 + diff.microseconds
+    return abs(diff.days * 24 * 60 * 60 * 1_000 + microseconds)
+
+
+def seconds_taken(diff):
+    """Convert a time diff to total number of seconds"""
+    return taken(diff) // 1_000
+
+
+def now():
+    """Convenience method to call datetime's now"""
+    return datetime.datetime.now()
+
+
+def microseconds_since_epoch():
+    """Number of microseconds since the epoch"""
+    return taken(now() - epoch)
+
+
+def seconds_since_epoch():
+    """Number of seconds since the epoch"""
+    return seconds_taken(now() - epoch)
