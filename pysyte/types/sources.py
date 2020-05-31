fab3f24fa8447b73869b8b85d36ec86cd937e008
J Alan Brogan <github@al-got-rhythm.net>
2020-05-31 17:07:07 +0100

    add sources

--- pysyte/types/sources.py
+++ pysyte/types/sources.py
@@ -1,8 +1,29 @@
 """Handle source files for pysyte"""
 
-from pysyte.types.paths import path
+from dataclasses import dataclass
+from functools import singledispatch
+
+from pysyte.types import paths
 
 
+@singledispatch
 def source(str_: str):
-    path_ = path(str_)
+    breakpoint()
+    return source(paths.path(str_))
 
+
+@source.register(type(None))
+def _(arg):
+    return source('')
+
+@dataclass
+class Source:
+    path: paths.FilePath
+
+@source.register(type(paths.FilePath))
+def _(arg):
+    return Source(arg)
+
+
+def sources(strings_: list):
+    return (source(_) for _ in strings_)
