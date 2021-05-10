9eb09783d1317c77d65239e1d4c5aceb6eb1bb4b
J Alan Brogan <github@al-got-rhythm.net>
2021-05-10 22:33:56 +0100

    Add method to clarify localhost data

--- pysyte/net/hosts.py
+++ pysyte/net/hosts.py
@@ -3,16 +3,21 @@
 import getpass
 import socket
 from dataclasses import dataclass
+from typing import List
 
 
 @dataclass
 class Host:
     hostname: str
-    aliases: list
-    addresses: list
-    users: list
+    aliases: List[str]
+    addresses: List[str]
+    users: List[str]
 
 
-localhost = Host(
-    *(list(socket.gethostbyname_ex(socket.gethostname())) + [getpass.getuser()])
-)
+def _read_localhost() -> Host:
+    """Read host values for localhost"""
+    host, aliases, addresses = socket.gethostbyname_ex(socket.gethostname())
+    user = getpass.getuser()
+    return Host(host, aliases, addresses, [user])
+
+localhost = _read_localhost()
