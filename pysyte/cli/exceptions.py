709a43d46c3aa0fd6644b324ca827c413bc5cc3a
J Alan Brogan <github@al-got-rhythm.net>
2022-08-09 01:41:11 +0100

    Require rich

---
+++ pysyte/cli/exceptions.py
@@ -0,0 +1,20 @@
+import sys
+
+from contextlib import contextmanager
+
+from rich.console import Console
+from rich.traceback import install
+
+install(show_locals=True)
+
+
+_console = Console()
+
+@contextmanager
+def rich_exceptions(locals_: dict):
+    if locals_:
+        locals().update(locals_)
+    try:
+        yield
+    except:
+        _console.print_exception(show_locals=True)
