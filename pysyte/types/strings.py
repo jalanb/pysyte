3e68255278bb4178e9f0244df058c32da79d600d
J Alan Brogan <github@al-got-rhythm.net>
2023-02-01 23:31:54 +0000

    Add Stringer Mixin

---
+++ pysyte/types/strings.py
@@ -0,0 +1,31 @@
+"""Miscellaneous string handlers"""
+
+from functools import total_ordering
+
+
+@total_ordering
+class Stringer:
+    """A Mixin to provide more ops to a class that can str()"""
+
+    def __str__(self):
+        raise NotImplementedError
+
+    def __repr__(self) -> str:
+        string = str(self)
+        return f'<{self.__class__.__name__} "{string}">'
+
+    def __hash__(self):
+        """Derive a hash value from the string"""
+        return hash(str(self))
+
+    def __eq__(self, other):
+        """Whether this equals other, compared as strings"""
+        return str(self) == str(other)
+
+    def __ne__(self, other):
+        """Whether this differs from other, compared as strings"""
+        return str(self) != str(other)
+
+    def __lt__(self, other):
+        """Whether this is less than other, compared as strings"""
+        return str(self) < str(other)
