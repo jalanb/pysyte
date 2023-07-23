960188f75d7cd31258392ede8384c71c00e6d851
J Alan Brogan <github@al-got-rhythm.net>
2023-07-23 23:17:48 +0100

    Add line args to kat

--- pysyte/kat/__main__.py
+++ pysyte/kat/__main__.py
@@ -4,19 +4,22 @@ Usage: python3 -m pysyte.kat [options] [files]
 
 """
 
+from pysyte.cli import arguments
+from pysyte.cli import lines
 from pysyte.cli.app import App
-from pysyte.cli.lines import arg_lines
 
 
-def args(app):
-    return (app.args.files("files"),)
+def parse_args():
+    parser = arguments.parser(__doc__)
+    line_parser = lines.add_args(parser)
+    return line_parser.parse_args()
 
 
 def kat(app):
     """Run kat"""
     for file in app.args.files():
         text = file.read()
-        start, lines_in = arg_lines(text, args)
+        start, lines_in = arg_lines(text, app.args)
         lines_out = args.sed(lines_in, start)
         text_ = "\n".join(lines_out)
         print(f"{text_}\n")
@@ -24,7 +27,8 @@ def kat(app):
 
 
 def main():
-    with App(kat) as app:
+    args = parse_args()
+    with App(kat, args) as app:
         app.run(app.args)
 
 
