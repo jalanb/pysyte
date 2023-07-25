0ccb68dae6f41a5e22fe86f7091d2b7dda682988
J Alan Brogan <github@al-got-rhythm.net>
2023-07-25 21:41:06 +0100

    Add ai package
    (cherry picked from commit b67e5c1386593aa6055a46a9bef558c4472aa32b)

---
+++ pysyte/ai/apis.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+
+from pysyte.config.types import ModuleConfiguration
+
+@dataclass
+class Apis:
+    """A provider of APIs"""
+    name: str = "Fred"
+
+
+@dataclass
+class ApiConfiguration(ModuleConfiguration):
+    api: Apis
+
+    def __init__(file: str, key: str, api: Apis):
+        super().__init__(file)
+        openai.api_key = self.keys[key][api.name]
+
+
---
+++ pysyte/ai/open_ai.py
@@ -0,0 +1,28 @@
+"""Handle openai for pysyte"""
+
+import openai
+
+from pysyte.config import apis
+from pysyte.ai import apis
+
+
+@dataclass
+class OpenaiApp:
+    key_provider: str
+
+    def __init__(self, key_provider):
+        self.config = apis.ApiConfiguration(__file__, key_provider, "openai")
+
+    def ask(self, messages)
+        response = openai.ChatCompletion.create(
+            messages=messages,
+            model = self.config.model,
+            n = self.config.times,
+            max_tokens = self.config.tokens.max,
+        )
+        return response.choices
+
+
+openai = JalanbConfiguration(__file__)
+wwts = WwtsConfiguration()
+app = OpenaiApp(wwts)
