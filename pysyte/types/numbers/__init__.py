9fe0d7b2106681f1fd06f2e903dbe05a9d271bdc
J Alan Brogan <github@al-got-rhythm.net>
2023-07-25 22:16:08 +0100

    Add one, two, many, lots
    (cherry picked from commit a63330fa57e93c1ea771730a261e25aaa3c90f6f)

---
+++ pysyte/types/numbers/__init__.py
@@ -0,0 +1,60 @@
+"""This module handles numbers for pysyte
+
+>>> from pysyte.types import numbers
+
+integers and strings work like int() for both
+>>> assert numbers.as_int(5) == 5 == numbers.as_int(" 5  ")
+>>> assert numbers.inty(5) == 5 == numbers.inty("5   ")
+
+inty() does extra computation is done for things with a length
+
+>>> assert numbers.inty("lll") == 3
+>>> assert numbers.inty([3,]) == 1
+
+as_int() does not
+>>> try:
+...     numbers.as_int("lll")
+...     assert False
+... except numbers.NAN:
+...     pass
+>>> try:
+...     numbers.as_int([3, ])
+...     assert False
+... except numbers.NAN:
+...     pass
+"""
+
+
+class NAN(ValueError):
+    def __init__(self, value):
+        super().__init__(f"NAN: {value}")
+
+
+def _to_number(value, kind, default):
+    """Convert the value to that kind of int, using that default
+
+    >>> assert _to_number(3, int, 7) == 3
+    >>> assert _to_number(3, float, 7) == 3.0
+    >>> assert _to_number(None, int, 7) == 7
+    >>> assert _to_number(None, float, 7) == 7.0
+    >>> assert _to_number(3, int, None) == 3
+    >>> assert _to_number(3, float, None) == 3.0
+    """
+    try:
+        return kind(value)
+    except (ValueError, TypeError):
+        if default is None:
+            raise NAN(value)
+        if isinstance(value, (type(None), )):
+            return kind(default)
+        try:
+            return len(value)
+        except TypeError:
+            raise ValueError(value)
+
+
+as_int = lambda x: _to_number(x, int, None)
+as_float = lambda x: _to_number(x, float, None)
+
+inty = lambda x: _to_number(x, int, x)
+floaty = lambda x: _to_number(x, float, x)
---
+++ pysyte/types/numbers/lots.py
@@ -0,0 +1,112 @@
+"""handle numbers for pysyte
+
+>>> one = lots([''])
+>>> assert one.limit == 1
+>>> two = lots(["", ""])
+>>> assert two.limit == 2
+>>> three = lots([1, "7", 9])
+>>> assert three.limit == 3
+>>> four = lots([9, 5, 3, 0])
+>>> assert four.limit == 4
+
+>>> assert two + one == three
+
+"""
+from typing import Any
+
+from dataclasses import dataclass
+from dataclasses import field
+
+@dataclass
+class Nones(list):
+    args: list = field(default_factory=list)
+
+    def __post_init__(self):
+        breakpoint()
+        self.limit = 0
+        self.check_args()
+
+    def check_args(self):
+        try:
+            self.check(len(self.args))
+        except IndexError as e:
+            args = self.args
+            raise ValueError(f"{self.__class__.__name__}({args=!r}): Too many args")
+
+    def __int__(self):
+        return len(self.args)
+
+    def __str__(self):
+        return str(int(self))
+
+    def __repr__(self):
+        return f'<{self.__class__.__name__} {int(self)} {self.args}>'
+
+    def __add__(self, other):
+        return lots(self.args + other.args)
+
+    def __sub__(self, other):
+        return lots(self.args[-len(other.args):]) # + other.args)
+
+    def __eq__(self, other):
+        return  self.__class__.__name__ == other.__class__.__name__
+
+    def __lt__(self, other):
+        return int(self) < int(other)
+
+    def __getitem__(self, i) -> Any:
+        self.check(i)
+        return super().__getitem__(i)
+
+    @property
+    def max(self):
+        return 5
+
+    def check(self, i) -> bool:
+        if self.limit >= self.max:
+            return True
+        if len(self.args) > self.limit:
+            raise IndexError(f"{self.__class__.__name__} has nothing at {i=}")
+        return True
+        raise TypeError(f"{self.__class__.__name__} has nothing at {i=}")
+
+
+class One(Nones):
+
+    def __post_init__(self):
+        self.limit = 1
+        self.check_args()
+
+
+class Two(Nones):
+
+    def __post_init__(self):
+        self.limit = 2
+        self.check_args()
+
+
+class Many(Two):
+
+    def __post_init__(self):
+        self.limit = 4
+        self.check_args()
+
+class Lots(Many):
+
+    def __post_init__(self):
+        self.limit = self.max + 1
+        self.check_args()
+
+
+def lots(args: list, many = 3):
+    a = len(args)
+    match a:
+        case 0:
+            return None
+        case 1:
+            return One(args)
+        case 2:
+            return Two(args)
+    if a <= many:
+        return Many(args)
+    return Lots(args)
