a599ede4703f98fdf6abf9db2772db20f7b235a1
J Alan Brogan <github@al-got-rhythm.net>
2023-10-30 08:37:22 +0000

    Add doctests for pysyte.types.time.times

--- pysyte/types/time/times.py
+++ pysyte/types/time/times.py
@@ -1,4 +1,18 @@
-"""Handle datetime for pysyte types"""
+"""Handle datetime for pysyte
+
+
+    >>> from pysyte.types.time import times as this
+    >>> dt = datetime.datetime
+
+
+    >>> assert this.seconds_since_epoch()      > 1_000_000_000
+    >>> assert this.microseconds_since_epoch() > 1_000_000_000_000
+    >>> one = dt(1979, 12, 25, 1, 2, 3)
+    >>> two = dt(1979, 12, 25, 1, 2, 4)
+    >>> a_second_past = two - one
+    >>> assert this.seconds_taken(a_second_past) == 1
+    >>> assert this.taken(a_second_past) == 1000
+"""
 
 import datetime
 import time
