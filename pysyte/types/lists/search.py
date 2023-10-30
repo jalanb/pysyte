02cd93ba95b6438ed52491465b74317619a71748
J Alan Brogan <github@al-got-rhythm.net>
2023-10-30 08:08:48 +0000

    Exapand types.lists into a package
    Add types.lists.search

--- pysyte/types/lists/search.py
+++ pysyte/types/lists/search.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from random import randint
+from random import shuffle
 from functools import partial
 from typing import Callable, List
 
@@ -51,8 +53,7 @@ def average(lo: int, hi: int) -> int:
 @dataclass
 class Chooser:
     """A context manager for choosers"""
-
-    chooser: Callable
+    chooser: Callable = None
 
     def choose(self, a, b):
         return self.chooser(a, b)
@@ -61,8 +62,7 @@ class Chooser:
 @dataclass
 class Picker:
     """A context manager for pickers"""
-
-    picker: Callable
+    picker: Callable = None
 
     def __post_init__(self):
         pass
@@ -99,9 +99,8 @@ def directed_search(average: Picker, arr: List[int], sought: int) -> int:
         -1
         >>> i = 1024
         >>> items = list(range(i))
-        >>> import random
-        >>> j = random.randint(0, i)
-        >>> random.shuffle(items)
+        >>> j = randint(0, i)
+        >>> shuffle(items)
         >>> assert binary_search(items, j) < i
     """
     lo, hi = 0, len(arr) - 1
@@ -109,6 +108,7 @@ def directed_search(average: Picker, arr: List[int], sought: int) -> int:
     while lo <= hi:
         i = average.pick(lo, hi)
         item = arr[i]
+        next_ = average.next_(item, sought)
         if item == sought:
             return i
         elif item < sought:
@@ -118,11 +118,9 @@ def directed_search(average: Picker, arr: List[int], sought: int) -> int:
 
     return -1
 
-
 binary_search = partial(directed_search, Picker(average))
 
 # Doctest
 if __name__ == "__main__":
     import doctest
-
     doctest.testmod()
