745346c7004c12296eb74e0e6ad904d63413e5ab
J Alan Brogan <github@al-got-rhythm.net>
2025-06-11 00:15:27 +0100

    Do not shadow super().split()

--- pysyte/types/paths.py
+++ pysyte/types/paths.py
@@ -360,7 +360,7 @@ class DotPath(StringPath):
         >>> p = DotPath('/path/to/x.py')
         >>> assert p.paths == p.dirpaths()
         """
-        parts = self.split()
+        parts = self.path_split()
         result = [DotPath(parts[0] or "/")]
         for name in parts[1:]:
             result.append(result[-1] / name)
@@ -409,8 +409,6 @@ class DotPath(StringPath):
         parts = super().split(separator, maxsplit)
         parts[0] = makepath(parts[0] if parts[0] else "/")
         return parts
-
-    split = path_split
 
     def abspath(self):
         return makepath(os.path.abspath(str(self)))
@@ -976,14 +974,14 @@ def as_path(string_or_path):
     return makepath(string_or_path)
 
 
-def string_to_paths(string) -> List[StringPath]:
+def string_to_paths(string: str) -> List[StringPath]:
     for c in ":, ;":
         if c in string:
             return strings_to_paths(string.split(c))
     return [makepath(string)]
 
 
-def strings_to_paths(strings) -> List[StringPath]:
+def strings_to_paths(strings: List[str]) -> List[StringPath]:
     return [makepath(s) for s in strings]
 
 
@@ -1022,7 +1020,7 @@ def pwd():
     return makepath(os.getcwd())
 
 
-def first_dir(path_string):
+def first_dir(path_string: str):
     """Get the first directory in that path
 
     >>> first_dir('usr/local/bin') == 'usr'
